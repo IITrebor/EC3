20fca25b13dc51f07ec31056bba2a29df569d77d
IITrebor <135287101+IITrebor@users.noreply.github.com>
2023-07-28 03:47:16 +0000

    Tercera parte

--- src/main/java/com/example/demo/CategoriaRepository.java
+++ src/main/java/com/example/demo/CategoriaRepository.java
@@ -7,9 +7,9 @@ import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(collectionResourceRel = "carreras", path = "carreras")
-public interface CarreraRepository extends PagingAndSortingRepository<Carrera, Integer>, CrudRepository<Carrera, Integer> {
+@RepositoryRestResource(collectionResourceRel = "categorias", path = "categorias")
+public interface CategoriaRepository extends PagingAndSortingRepository<Categoria, Integer>, CrudRepository<Categoria, Integer> {
     
-    List<Curso> findByNombre(@Param("nombre") String nombre);
+    List<Instrumento> findByNombre(@Param("nombre") String nombre);
 
 }
